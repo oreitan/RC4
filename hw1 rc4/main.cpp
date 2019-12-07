41f2054aeb21dcdce9afc5163c2a56e23647e3d9
or eitan <oreitan@gmail.com>
2019-12-07 09:21:30 +0200

    Initial Commit

---
+++ hw1 rc4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hw1 rc4
+//
+//  Created by or eitan on 07/12/2019.
+//  Copyright © 2019 or eitan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
